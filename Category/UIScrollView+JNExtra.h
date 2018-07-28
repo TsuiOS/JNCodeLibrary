721307277480b9ded93c80cfde5c714abbc8f1ef
xuning <xuning@qq.com>
2018-07-28 15:08:36 +0800

    增加UIScrollView+JNExtra

---
+++ Category/UIScrollView+JNExtra.h
@@ -0,0 +1,18 @@
+//
+//  UIScrollView+JNExtra.h
+//  JNCodeLibrary
+//
+//  Created by xuning on 2018/7/28.
+//  Copyright © 2018年 Hsu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface UICollectionView (JNExtra)
+
+@end
+
+@interface UIScrollView (JNExtra)
+
+@end
---
+++ Category/UIScrollView+JNExtra.m
@@ -0,0 +1,87 @@
+//
+//  UIScrollView+JNExtra.m
+//  JNCodeLibrary
+//
+//  Created by xuning on 2018/7/28.
+//  Copyright © 2018年 Hsu. All rights reserved.
+//
+
+#import "UIScrollView+JNExtra.h"
+
+@implementation UICollectionView (JNExtra)
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    
+    if ([self panBack:gestureRecognizer]) {
+        return YES;
+    }
+    return NO;
+    
+}
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    
+    if ([self panBack:gestureRecognizer]) {
+        return NO;
+    }
+    return YES;
+    
+}
+
+- (BOOL)panBack:(UIGestureRecognizer *)gestureRecognizer {
+    
+    if (gestureRecognizer == self.panGestureRecognizer) {
+        UIPanGestureRecognizer *pan = (UIPanGestureRecognizer *)gestureRecognizer;
+        CGPoint point = [pan translationInView:self];
+        UIGestureRecognizerState state = gestureRecognizer.state;
+        if (UIGestureRecognizerStateBegan == state || UIGestureRecognizerStatePossible == state) {
+            CGPoint location = [gestureRecognizer locationInView:self];
+            if (point.x > 0 && location.x < 90 && self.contentOffset.x <= 0) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+    
+}
+
+
+@end
+
+@implementation UIScrollView (JNExtra)
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    
+    if ([self panBack:gestureRecognizer]) {
+        return YES;
+    }
+    return NO;
+    
+}
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+    
+    if ([self panBack:gestureRecognizer]) {
+        return NO;
+    }
+    return YES;
+    
+}
+
+- (BOOL)panBack:(UIGestureRecognizer *)gestureRecognizer {
+    
+    if (gestureRecognizer == self.panGestureRecognizer) {
+        UIPanGestureRecognizer *pan = (UIPanGestureRecognizer *)gestureRecognizer;
+        CGPoint point = [pan translationInView:self];
+        UIGestureRecognizerState state = gestureRecognizer.state;
+        if (UIGestureRecognizerStateBegan == state || UIGestureRecognizerStatePossible == state) {
+            CGPoint location = [gestureRecognizer locationInView:self];
+            if (point.x > 0 && location.x < 90 && self.contentOffset.x <= 0) {
+                return YES;
+            }
+        }
+    }
+    return NO;
+    
+}
+
+
+@end
